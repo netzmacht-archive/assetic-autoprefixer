f4826f72a8f44ef071b16f4c051e4bf6dd27b630
Tristan Lins <tristan.lins@bit3.de>
2014-09-03 20:25:10 +0200

    Switch to PSR-1/PSR-2 coding style.

--- src/AutoprefixerFilter.php
+++ src/AutoprefixerFilter.php
@@ -20,117 +20,115 @@ use Symfony\Component\Process\Exception\ProcessTimedOutException;
 class AutoprefixerFilter extends BaseProcessFilter
 {
 
-	protected $autoprefixerBin;
+    protected $autoprefixerBin;
 
-	private $cascade = true;
+    private $cascade = true;
 
-	private $safe = false;
+    private $safe = false;
 
-	public function __construct($autoprefixerBin = '/usr/bin/autoprefixer')
-	{
-		$this->autoprefixerBin = $autoprefixerBin;
-	}
+    public function __construct($autoprefixerBin = '/usr/bin/autoprefixer')
+    {
+        $this->autoprefixerBin = $autoprefixerBin;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getAutoprefixerBin()
-	{
-		return $this->autoprefixerBin;
-	}
+    /**
+     * @return string
+     */
+    public function getAutoprefixerBin()
+    {
+        return $this->autoprefixerBin;
+    }
 
-	/**
-	 * @param string $autoprefixerBin
-	 *
-	 * @return static
-	 */
-	public function setAutoprefixerBin($autoprefixerBin)
-	{
-		$this->autoprefixerBin = (string) $autoprefixerBin;
-		return $this;
-	}
+    /**
+     * @param string $autoprefixerBin
+     *
+     * @return static
+     */
+    public function setAutoprefixerBin($autoprefixerBin)
+    {
+        $this->autoprefixerBin = (string) $autoprefixerBin;
+        return $this;
+    }
 
-	/**
-	 * @return boolean
-	 */
-	public function isCascade()
-	{
-		return $this->cascade;
-	}
+    /**
+     * @return boolean
+     */
+    public function isCascade()
+    {
+        return $this->cascade;
+    }
 
-	/**
-	 * @param boolean $cascade
-	 *
-	 * @return static
-	 */
-	public function setCascade($cascade)
-	{
-		$this->cascade = (bool) $cascade;
-		return $this;
-	}
+    /**
+     * @param boolean $cascade
+     *
+     * @return static
+     */
+    public function setCascade($cascade)
+    {
+        $this->cascade = (bool) $cascade;
+        return $this;
+    }
 
-	/**
-	 * @return boolean
-	 */
-	public function isSafe()
-	{
-		return $this->safe;
-	}
+    /**
+     * @return boolean
+     */
+    public function isSafe()
+    {
+        return $this->safe;
+    }
 
-	/**
-	 * @param boolean $safe
-	 *
-	 * @return static
-	 */
-	public function setSafe($safe)
-	{
-		$this->safe = (bool) $safe;
-		return $this;
-	}
+    /**
+     * @param boolean $safe
+     *
+     * @return static
+     */
+    public function setSafe($safe)
+    {
+        $this->safe = (bool) $safe;
+        return $this;
+    }
 
-	public function filterLoad(AssetInterface $asset)
-	{
-		$input = tempnam(sys_get_temp_dir(), 'assetic_autoprefixer');
-		file_put_contents($input, $asset->getContent());
+    public function filterLoad(AssetInterface $asset)
+    {
+        $input = tempnam(sys_get_temp_dir(), 'assetic_autoprefixer');
+        file_put_contents($input, $asset->getContent());
 
-		$pb = $this->createProcessBuilder();
-		$pb->add($this->autoprefixerBin);
+        $pb = $this->createProcessBuilder();
+        $pb->add($this->autoprefixerBin);
 
-		// disable cascade
-		if (!$this->isCascade()) {
-			$pb->add('--no-cascade');
-		}
-		// enable safe mode
-		if ($this->isSafe()) {
-			$pb->add('--safe');
-		}
-		// output to stdout
-		$pb->add('-o')->add('-');
-		// input file
-		$pb->add($input);
+        // disable cascade
+        if (!$this->isCascade()) {
+            $pb->add('--no-cascade');
+        }
+        // enable safe mode
+        if ($this->isSafe()) {
+            $pb->add('--safe');
+        }
+        // output to stdout
+        $pb->add('-o')->add('-');
+        // input file
+        $pb->add($input);
 
-		try {
-			$process = $pb->getProcess();
-			$process->run();
-			unlink($input);
+        try {
+            $process = $pb->getProcess();
+            $process->run();
+            unlink($input);
 
-			if (!$process->isSuccessful()) {
-				throw FilterException::fromProcess($process)->setInput($asset->getContent());
-			}
-		}
-		catch (ProcessFailedException $exception) {
-			unlink($input);
-			throw $exception;
-		}
-		catch (ProcessTimedOutException $exception) {
-			unlink($input);
-			throw $exception;
-		}
+            if (!$process->isSuccessful()) {
+                throw FilterException::fromProcess($process)->setInput($asset->getContent());
+            }
+        } catch (ProcessFailedException $exception) {
+            unlink($input);
+            throw $exception;
+        } catch (ProcessTimedOutException $exception) {
+            unlink($input);
+            throw $exception;
+        }
 
-		$asset->setContent($process->getOutput());
-	}
+        $asset->setContent($process->getOutput());
+    }
 
-	public function filterDump(AssetInterface $asset)
-	{
-	}
+    public function filterDump(AssetInterface $asset)
+    {
+    }
 }
